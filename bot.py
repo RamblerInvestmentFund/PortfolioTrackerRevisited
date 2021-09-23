1fda8cf73133e318df88490728b46efb98b7dff1
anthonypeters <apeters10@luc.edu>
2021-09-22 20:46:30 -0500

    Formatted PDF with Cover and Extra Pages

--- bot.py
+++ bot.py
@@ -1,6 +1,45 @@
 import slack
+from fpdf import FPDF
 import os
 
-client = slack.WebClient(token = '####')
 
-client.chat_postMessage(channel = '#sector-materials', text='Hello World!')
+pdf = FPDF(orientation='landscape')
+pdf.add_page()
+pdf.set_fill_color(128, 0, 0)
+pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
+pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
+pdf.set_margin(0)
+pdf.image(x = -0.5, y = 12, w = pdf.w + 1, name='RIFLogo(B&W).png')
+
+pdf.add_page()
+pdf.set_fill_color(128, 0, 0)
+pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
+pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
+pdf.set_fill_color(0, 0, 0)
+pdf.rect(x = 0, y = 12, w = pdf.w, h = 186, style = 'F')
+
+pdf.add_page()
+pdf.set_fill_color(128, 0, 0)
+pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
+pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
+pdf.set_fill_color(0, 0, 0)
+pdf.rect(x = 0, y = 12, w = pdf.w, h = 186, style = 'F')
+
+
+#pdf.set_text_color(r=128, g=0, b=0)
+#pdf.set_font(family='Times', style='B', size=26)
+#pdf.cell(w=295, h=210, txt='The Rambler Investment Fund', align='C', center=True)
+
+
+
+
+
+
+
+pdf.output('pdf_1.pdf')
+
+
+
+#### Call Bot and Send Portfolio
+# client = slack.WebClient(token = '####')
+# client.chat_postMessage(channel = '#sector-materials', text='Hello World!')
