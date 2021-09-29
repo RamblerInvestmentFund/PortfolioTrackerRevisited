fd0bac09b7b32eeda504195fcbd03ce5e0d02552
anthonypeters <apeters10@luc.edu>
2021-09-29 10:22:27 -0500

    Fixed Benchmark

--- bot.py
+++ bot.py
@@ -8,9 +8,8 @@ import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-today = datetime.date.today()
 print('Running...')
+
 # ---- Creates DataFrame with Tickers, Shares, and Basis
 df = pd.read_csv(filepath_or_buffer='portfolio.csv', index_col=False)
 tickerString = []
@@ -31,6 +30,7 @@ historicalValue = historicalValue.mul(shares, axis=1)
 portfolioValue = pd.DataFrame(columns=['Portfolio'], index=historicalValue.index)
 portfolioValue['Portfolio'] = historicalValue.sum(axis=1)
 
+# ---- Creates Benchmark Portfolio Value
 totalHoldingStart = historicalValue.iloc[0]
 
 sumEquity = 0
@@ -55,78 +55,63 @@ for idx in totalHoldingStart.index:
             i+=1
 
 startSum = [sumEquity, sumREIT, sumCommodity, sumBond]
+ECRB_ETF = ['SPY', 'USCI', 'VNQ', 'TLT']
+benchmarkHistorical = yf.download(ECRB_ETF, start='2021-06-21')
+benchmarkHistorical = benchmarkHistorical['Close']
+benchmarkStart = benchmarkHistorical.iloc[0]
+benchmarkShares = [(sumEquity/benchmarkStart['SPY']), (sumBond/benchmarkStart['TLT']), (sumCommodity/benchmarkStart['USCI']), (sumREIT/benchmarkStart['VNQ'])]
+benchmarkHistorical = benchmarkHistorical.mul(benchmarkShares, axis=1)
+benchPortfolioValue = pd.DataFrame(columns=['Portfolio'], index=benchmarkHistorical.index)
+benchPortfolioValue['Portfolio'] = benchmarkHistorical.sum(axis=1)
+
+# ---- Creates Plot of Portfolio
+plt.style.use('Solarize_Light2')
+plt.plot(portfolioValue, color='red')
+plt.plot(benchPortfolioValue, color='blue')
+plt.xticks(rotation = 20)
+plt.ylabel('Portfolio Change')
+plt.xlabel('Days Since Rebalance')
+plt.legend(['RIF', 'Benchmark'])
+plt.title('Portfolio Growth Since The Rebalance V.S. Benchmark', y=1.05)
+plt.savefig('portfolioVSbenchmark.png')
 
 
-# ---- Create benchmark 
-# historicalValBench = pd.DataFrame(columns=['Equity', 'Commodity', 'REIT', 'Bond'])
+today = datetime.date.today()
+# ---- Cover Page
+pdf = FPDF(orientation='landscape')
+pdf.set_margin(2)
+pdf.add_page()
+pdf.set_fill_color(128, 0, 0)
+pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
+# ---- Date
+pdf.set_font("Times", size=22)
+pdf.set_font(style='B')
+pdf.set_text_color(237, 232, 228)
+pdf.cell(txt=str(today), align='C')
+# ----
+pdf.set_fill_color(128, 0, 0)
+pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
+pdf.image(x = -0.5, y = 12, w = pdf.w + 1, name='RIFLogo(B&W).png')
 
-# benchmarkShares = [(sumEquity/si.get_live_price(ticker='SPY')), (sumBond/si.get_live_price(ticker='TLT')), (sumCommodity/si.get_live_price(ticker='USCI')),(sumREIT/si.get_live_price(ticker='VNQ'))]
-# sumBasisSeries = pd.Series(basisList, index=(historicalValBench.columns))
-# basisPercentageSeries = pd.Series([(sumEquity/sumBasis), (sumCommodity/sumBasis), (sumREIT/sumBasis), (sumBond/sumBasis)], index=historicalValBench.columns)
-# historicalValBench = historicalValBench.append(sumBasisSeries, ignore_index=True)
-# historicalValBench = historicalValBench.append(basisPercentageSeries, ignore_index=True)
+# ---- Second Page
+pdf.add_page()
+pdf.set_fill_color(128, 0, 0)
+pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
+pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
+pdf.set_fill_color(0, 0, 0)
+pdf.rect(x = 0, y = 12, w = pdf.w, h = 186, style = 'F')
+pdf.image(x=1, y=13, w=(pdf.w+1)/2, name='portfolioVSbenchmark.png')
 
-# ECRB_ETF = ['SPY', 'USCI', 'VNQ', 'TLT']
-# benchmarkHistorical = yf.download(ECRB_ETF, start='2021-06-21')
-# benchmarkHistorical = benchmarkHistorical['Close']
-# benchmarkHistorical = benchmarkHistorical.mul(benchmarkShares, axis=1)
-# benchPortfolioValue = pd.DataFrame(columns=['Portfolio'], index=benchmarkHistorical.index)
-# benchPortfolioValue['Portfolio'] = benchmarkHistorical.sum(axis=1)
-# # !!!!!!!! Benchmark is wrong. Needs to instead, find the sum of the basis at the beginning of the rebalance,
-# # then it needs to put those sums into the sumBasisSeries, into the Historicalvalbench, calculate benchmark shares by dividing each
-# # sector sum by the price at start of rebalance, not at the end. Then multiply those shares throughout the table. Sum the rows of the table, and plot
+# ---- Third Page
+pdf.add_page()
+pdf.set_fill_color(128, 0, 0)
+pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
+pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
+pdf.set_fill_color(0, 0, 0)
+pdf.rect(x = 0, y = 12, w = pdf.w + 1, h = 186, style = 'F')
 
-
-
-# # ---- Creates Plot of Portfolio
-# plt.style.use('Solarize_Light2')
-# plt.plot(portfolioValue, color='red')
-# plt.plot(benchPortfolioValue, color='blue')
-# plt.xticks(rotation = 20)
-# plt.ylabel('Portfolio Change')
-# plt.xlabel('Days Since Rebalance')
-# plt.legend(['RIF', 'Benchmark'])
-# plt.title('Portfolio Growth Since The Rebalance V.S. Benchmark', y=1.05)
-# plt.show()
-# plt.savefig('portfolioVSbenchmark.png')
-
-
-# # ---- Cover Page
-
-# pdf = FPDF(orientation='landscape')
-# pdf.set_margin(2)
-# pdf.add_page()
-# pdf.set_fill_color(128, 0, 0)
-# pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
-# # ---- Date
-# pdf.set_font("Times", size=22)
-# pdf.set_font(style='B')
-# pdf.set_text_color(237, 232, 228)
-# pdf.cell(txt=str(today), align='C')
-# # ----
-# pdf.set_fill_color(128, 0, 0)
-# pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
-# pdf.image(x = -0.5, y = 12, w = pdf.w + 1, name='RIFLogo(B&W).png')
-
-# # ---- Second Page
-# pdf.add_page()
-# pdf.set_fill_color(128, 0, 0)
-# pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
-# pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
-# pdf.set_fill_color(0, 0, 0)
-# pdf.rect(x = 0, y = 12, w = pdf.w, h = 186, style = 'F')
-# pdf.image(x=-0.5, y=12, w=(pdf.w+1)/2, name='portfolioVSbenchmark.png')
-
-# # ---- Third Page
-# pdf.add_page()
-# pdf.set_fill_color(128, 0, 0)
-# pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
-# pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
-# pdf.set_fill_color(0, 0, 0)
-# pdf.rect(x = 0, y = 12, w = pdf.w + 1, h = 186, style = 'F')
-
-# # ---- PDF Output
-# pdf.output('pdf_1.pdf')
+# ---- PDF Output
+pdf.output('pdf_1.pdf')
 
 
 
