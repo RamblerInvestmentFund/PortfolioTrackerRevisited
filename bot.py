53f25df06e7bae25bfbad11ba7aa5ca93c77dd1b
tauruskevicius <tauruskevicius@regis.edu>
2021-10-27 09:59:13 -0500

    Created performance.py module

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import data
 import benchmark
+import performance
 import quantstats as qs
 
 
@@ -28,20 +29,11 @@ print(portfolioValueExtended)
 
 qs.reports.html(portfolioValueExtended['Portfolio'], "SPY", output='rif_vs_bench.html')
 
-# Individual graphs from .html report, no option for Cumulative Returns v Benc (Volatility Matched)
-qs.plots.returns(portfolioValueExtended['Portfolio'], "SPY", savefig='Figures/YTD_rif_vs_bench.png')
-qs.plots.log_returns(portfolioValueExtended['Portfolio'], "SPY", savefig='Figures/YTD_rif_vs_bench_log.png')
-qs.plots.yearly_returns(portfolioValueExtended['Portfolio'], "SPY", savefig='Figures/EOY_rif_vs_bench.png')
-qs.plots.histogram(portfolioValueExtended['Portfolio'],savefig='Figures/rif_monthly_dist.png')
-qs.plots.daily_returns(portfolioValueExtended['Portfolio'],savefig='Figures/rif_daily_returns.png')
-qs.plots.rolling_beta(portfolioValueExtended['Portfolio'], "SPY", savefig='Figures/rol_beta.png')
-qs.plots.rolling_volatility(portfolioValueExtended['Portfolio'], "SPY", savefig='Figures/rol_vol.png')
-qs.plots.rolling_sharpe(portfolioValueExtended['Portfolio'], "SPY", savefig='Figures/rol_beta_sharpe.png')
-qs.plots.rolling_sortino(portfolioValueExtended['Portfolio'], "SPY", savefig='Figures/rol_beta_sortino.png')
-qs.plots.drawdowns_periods(portfolioValueExtended['Portfolio'], savefig='Figures/drawdown_periods.png')
-qs.plots.drawdown(portfolioValueExtended['Portfolio'], savefig='Figures/drawdown_plot.png')
-qs.plots.monthly_returns(portfolioValueExtended['Portfolio'], savefig='Figures/monthly_returns.png')
-qs.plots.distribution(portfolioValueExtended['Portfolio'], savefig='Figures/return_quantiles.png')
+# ---- Extracting individual graphs from .html report
+performance.create_performance_plots(portfolioValueExtended)
+
+# ----Creating  a correlation matrix of returns
+
 
 
 # #### ---------------- Creating PDF ---------------- ####
