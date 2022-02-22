4f49fc865718540510961d04a97eb7eff4f7978f
Matthew Hyatt <mhyatt000@gmail.com>
2022-02-21 20:44:56 -0600

    add main.py

--- bot.py
+++ bot.py
@@ -15,20 +15,20 @@ import metrics
 class Document(FPDF):
     '''extended FPDF class with custom functions'''
 
-    WHITE = (255,255,255)
-    BLACK = (0,0,0)
-    HALF_RED = (128,0,0)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    HALF_RED = (128, 0, 0)
     GREEN = (0, 255, 0)
     RED = (255, 0, 0)
 
-    def banner(self, *,top):
+    def banner(self, *, top):
         '''
         creates a dark red banner at the top or bottom of the page
         '''
 
         self.set_fill_color(*self.HALF_RED)
         y = 0 if top else 198
-        self.rect(x = 0, y = y, w = self.w, h = 12, style = 'F')
+        self.rect(x=0, y=y, w=self.w, h=12, style='F')
 
     def add_page_banners(self):
         '''creates a new page with banners at the top and bottom'''
@@ -36,14 +36,16 @@ class Document(FPDF):
         self.add_page()
         self.banner(top=True)
         self.set_fill_color(*self.BLACK)
-        self.rect(x = 0, y = 12, w = self.w, h = 186, style = 'F')
+        self.rect(x=0, y=12, w=self.w, h=186, style='F')
         self.banner(top=False)
+
 
 def build_pdf():
     '''generates pdf report'''
 
     '''TODO: write to json for quicker intermediate loading ... try excepts'''
-    df, portfolioValue, historicalValue, portfolioValueExtended = data.pull_data('assets/portfolio.csv')
+    df, portfolioValue, historicalValue, portfolioValueExtended = data.pull_data(
+        'assets/portfolio.csv')
     totalHoldingStart, benchPortfolioValue = benchmark.create_benchmark(df, historicalValue)
     benchmark.plot_benchmark(portfolioValue, benchPortfolioValue)
     topGainers, topLosers = data.topGainersLosers(historicalValue, totalHoldingStart)
@@ -51,8 +53,9 @@ def build_pdf():
     metrics.create_performance_plots(portfolioValueExtended)
     metrics.create_corr_heatmap(list(df['Shares']), historicalValue)
 
-    #qs.extend_pandas()
-    #qs.reports.html(portfolioValueExtended['Portfolio'], "SPY", output='rif_vs_bench.html')
+    # # writes html
+    # qs.extend_pandas()
+    # qs.reports.html(portfolioValueExtended['Portfolio'], "SPY", output='rif_vs_bench.html')
 
     'Cover Page'
     pdf = Document(orientation='landscape')
@@ -61,9 +64,9 @@ def build_pdf():
 
     pdf.set_font("Times", size=20, style='B')
     pdf.set_text_color(*pdf.WHITE)
-    pdf.cell(0,-5,txt=str(datetime.date.today()), align='L')
+    pdf.cell(0, -5, txt=str(datetime.date.today()), align='L')
 
-    pdf.image(x = -0.5, y = 12, w = pdf.w + 1, name='assets/RIFLogo(B&W).png')
+    pdf.image(x=-0.5, y=12, w=pdf.w + 1, name='assets/RIFLogo(B&W).png')
 
     'Second Page'
     pdf.add_page_banners()
@@ -75,7 +78,8 @@ def build_pdf():
     port = portfolioValue['Portfolio']
     percentGain = round(100*(port[-1] - port[0])/port[0], 2)
 
-    pdf.cell(((pdf.w/2) + (pdf.w/2 - 125)), 8, f'Performance for the current period: {percentGain}%', ln=1, align='C')
+    pdf.cell(((pdf.w/2) + (pdf.w/2 - 125)), 8,
+             f'Performance for the current period: {percentGain}%', ln=1, align='C')
 
     losers = list(topLosers.to_records())
     gainers = list(topGainers.to_records())
@@ -83,7 +87,7 @@ def build_pdf():
     losers, gainers = losers[:minlen], gainers[:minlen]
 
     pdf.set_font("Times", size=14)
-    for gainer,loser in zip(gainers, losers):
+    for gainer, loser in zip(gainers, losers):
         pdf.set_text_color(*pdf.GREEN)
         pdf.cell(25, 8, gainer[0])
         pdf.cell(50, 8, gainer[1])
@@ -96,20 +100,20 @@ def build_pdf():
 
     'Third Page'
     pdf.add_page_banners()
-    pdf.image(x = -0.5, y = 12, w = pdf.w + 1, name='img/monthly_returns.png')
+    pdf.image(x=-0.5, y=12, w=pdf.w + 1, name='img/monthly_returns.png')
 
     'Fourth Page'
     pdf.add_page_banners()
-    pdf.image(x = -0.5, y = 12, w = pdf.w + 1, name='img/return_quantiles.png')
+    pdf.image(x=-0.5, y=12, w=pdf.w + 1, name='img/return_quantiles.png')
 
     'Fifth Page'
     pdf.add_page_banners()
-    pdf.image(x = -0.5, y = 12, w = pdf.w + 1, name='img/portfolio_corr.png')
+    pdf.image(x=-0.5, y=12, w=pdf.w + 1, name='img/portfolio_corr.png')
 
     'Sixth Page'
     pdf.add_page_banners()
-    pdf.image(x = -0.5, y = pdf.h-100, w = pdf.w + 1, name='img/rol_beta_sharpe.png')
-    pdf.image(x = -0.5, y = 12, w = pdf.w + 1, name='img/rol_beta_sortino.png')
+    pdf.image(x=-0.5, y=pdf.h-100, w=pdf.w + 1, name='img/rol_beta_sharpe.png')
+    pdf.image(x=-0.5, y=12, w=pdf.w + 1, name='img/rol_beta_sortino.png')
 
     pdf.output('assets/testpdf.pdf')
 
@@ -118,8 +122,10 @@ def build_pdf():
     # client = slack.WebClient(token = '####')
     # client.files_upload(channels = '#sector-materials', file='assets/testpdf.pdf')
 
+
 def main():
     build_pdf()
+
 
 if __name__ == '__main__':
     main()
---
+++ main.py
@@ -0,0 +1,41 @@
+"""generates html tearsheet"""
+
+import quantstats as qs
+
+from data import pull_data
+
+
+def tearsheet():
+    """generates html tearsheet"""
+
+    """TODO
+    - portfolio.csv file
+        - update for this semester
+        - currently just have 2020
+    - go to slack for history google slides
+    - portfolio semester [1..4]
+        - read it seperately
+        - then concatenate
+        - name them as [equity .. REIT] if convenient otw dw
+
+    nextsteps:
+        - all available methods
+            [f for f in dir(qs.stats) if f[0] != '_']
+            [f for f in dir(qs.plots) if f[0] != '_']
+    """
+
+    *_, portfolioValueExtended = pull_data("assets/portfolio.csv")
+    port = portfolioValueExtended['Portfolio']
+
+    # writes html
+    qs.extend_pandas()
+    filename = "rif_vs_bench.html"
+    qs.reports.html(port, "SPY", output=filename, download_filename=filename)
+
+
+def main():
+    tearsheet()
+
+
+if __name__ == "__main__":
+    main()
